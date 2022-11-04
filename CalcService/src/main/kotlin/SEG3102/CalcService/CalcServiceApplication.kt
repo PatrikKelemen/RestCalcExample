8bb7b46cab0ff08c82fd236926233a00e1afffb1
Patrik <PatrikLazsloKelemen@gmail.com>
2022-11-04 00:47:55 -0400

    Lab setup

---
+++ CalcService/src/main/kotlin/SEG3102/CalcService/CalcServiceApplication.kt
@@ -0,0 +1,11 @@
+package SEG3102.CalcService
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CalcServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<CalcServiceApplication>(*args)
+}
---
+++ CalcService/src/test/kotlin/SEG3102/CalcService/CalcServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package SEG3102.CalcService
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CalcServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
